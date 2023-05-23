e34cef7d76e8105d54f2527354fd73f547e7fca6
Adityan <67089770+Adityan-compile@users.noreply.github.com>
2023-05-23 13:28:15 +0530

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentPage;
-let query;
+let query = null;
 
 function updateCurrentPage(pageNo){
     document.getElementById("currentPage").innerText = pageNo;
@@ -13,7 +13,18 @@ window.onload = ()=>{
       query = new URLSearchParams(window.location.search).get('q');
 
       document.querySelector('.gsc-control-cse').style = "display:none;";
-      document.getElementById("search-box").value = query; 
+      document.getElementById("search-box").value = query && ""; 
+
+      let nav = document.getElementsByClassName("nav-section");
+      
+
+      if(query === null){
+        nav[0].style.display = 'none';
+        nav[1].style.display = 'none';
+      }else{
+        nav[0].style.display = 'flex';
+        nav[1].style.display = 'flex';
+      }
 
 }
 
@@ -46,7 +57,7 @@ function nextPage(){
 }
 
 function updateExtBtn(){
-    document.getElementById("ext-query").innerHTML = new URLSearchParams(window.location.search).get('q');
+    document.getElementById("ext-query").innerHTML = `&nbsp;${new URLSearchParams(window.location.search).get('q')}&nbsp;`;
 }
 
 window.addEventListener('hashchange', e=>{
@@ -77,10 +88,11 @@ const myWebSearchStartingCallback = (gname, query) =>
 
 const myResultsReadyCallback = function (name, q, promos, results, resultsDiv) {
   let processedResults = [];
-  console.log(results);
+
   // Clean up Results
   results.forEach((el) => {
     if (
+      el.visibleUrl === "www.youtube.com" &&
       "videoobject" in el.richSnippet &&
       el.richSnippet.videoobject.genre === "Music"
     ) {
@@ -88,9 +100,10 @@ const myResultsReadyCallback = function (name, q, promos, results, resultsDiv) {
         owner: (el.richSnippet.person && el.richSnippet.person.name) || "",
         embed: el.richSnippet.videoobject.embedurl,
         views: parseInt(el.richSnippet.videoobject.interactioncount),
-        title: el.titleNoFormatting.slice(0,20)+'...',
+        title: el.titleNoFormatting,
         thumbnail: el.richSnippet.videoobject.thumbnailurl,
         duration: el.richSnippet.videoobject.duration,
+        url: el.richSnippet.videoobject.url
       });
     }
   });
@@ -113,15 +126,18 @@ const myResultsReadyCallback = function (name, q, promos, results, resultsDiv) {
 
   let resultContainer = document.getElementById("results");
 
-  processedResults.forEach(result=>{
-    let resultTemplate = `<div class="flex justify-start result">
+  processedResults.forEach((result,idx)=>{
+
+    let resultTemplate = `<div id="result-${idx}" class="flex justify-start result br-10" onclick="openPreview('result-${idx}')" data-target='${JSON.stringify(result)}'>
     <div class="thumb-container">
-    <img class="thumbnail" src="${result.thumbnail}">
+    <object class="thumbnail br-10" data="${result.thumbnail}">
+      <img class="thumbnail br-10" src="alt-thumb.png">
+    </object>
     <div class="duration">${result.duration}</div>
     </div>
-    <div class="flex col w-100 align-start m-10">
+    <div class="flex col w-100 align-start space-between m-10">
         <div>
-            <h4 style="margin-bottom:0;">${result.title}</h3>
+            <h4 style="margin-bottom:0;">${result.title.slice(0,20)+'...'}</h3>
             <h6 style="margin-top:0;">${result.owner}</h5>
         </div>
         <div class="flex row space-between w-100">
@@ -159,4 +175,22 @@ window.__gcse = {
 
 function openInGoogle(){
   window.open("https://www.google.com/search?q="+query, '_blank');
+}
+
+function openPreview(result){
+    const resultObj = JSON.parse(document.getElementById(result).getAttribute('data-target'));
+    
+    document.getElementById('player').src = resultObj.embed;
+    document.getElementById('title').innerText = resultObj.title;
+    document.getElementById('owner').innerText = ' ' + resultObj.owner;
+    document.getElementById('views').innerHTML = resultObj.views + "views";
+    document.getElementById('visit').onclick = () => window.open(resultObj.url, '_blank');
+
+    document.getElementById('preview-overlay').style.display = 'flex';
+    document.getElementById('main-content').style.display = 'none';
+}
+
+function closePreview(){
+  document.getElementById('preview-overlay').style.display = 'none';
+  document.getElementById('main-content').style.display = 'flex';
 }
